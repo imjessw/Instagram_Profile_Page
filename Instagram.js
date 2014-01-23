0b5f1e93e77a2276559a60e27d6d846e71e07f38
Intern Jess <internjess@Michele-Muses-Computer.local>
2014-01-23 00:28:35 -0500

    Fixed  Javascript scripts and added animation

--- Instagram.js
+++ Instagram.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
+$(function() {
 
+	console.log("HELLLLLOOOOOOO")
+
+	$(".picture").hover(function(){
+	    $(this).animate({ width: "500px" });
+	    $(this).animate({ height: "500px" });
+	}, function() {
+	    $(this).animate({ width: "200px" });
+	    $(this).animate({ height: "200px" });
+	});
 	// $("picture").mouseover(function(){
 	// 	var $("picture")= $(this)
 	// 	$(this).animate({ width: "600px" });
@@ -9,16 +18,18 @@ $(document).ready(function(){
 	// 	});
 	// }
 
-	
-	$("img").hover(function() {
-    $(this).height(function(i, h) { return h * 3; })
-           .width(function(i, w) { return w * 3; });
-}, function() {
-    $(this).height(function(i, h) {
-        return h / 3;
-    }).width(function(i, w) {
-        return w / 3;
-    })
+
+
+// 	$("img").hover(function() {
+//     $(this).height(function(i, h) { return h * 3; })
+//            .width(function(i, w) { return w * 3; });
+// }, function() {
+//     $(this).height(function(i, h) {
+//         return h / 3;
+//     }).width(function(i, w) {
+//         return w / 3;
+//     })
+	console.log("HELLLLLOOOOOOO 22222")
 });
 
 
